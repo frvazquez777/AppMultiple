aec527986ea7497f1562b208ae4d771285852dd3
fredy vazquez <crazzy-rock@live.com.mx>
2019-02-01 11:02:32 -0600

    Se agrega validacion para la navegacion de segue

--- AppMultiple/MainController.swift
+++ AppMultiple/MainController.swift
@@ -14,7 +14,6 @@ class MainController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         print("AppMultiple Init")
   
     }
@@ -49,17 +48,19 @@ class MainController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    func getSegueValidation(segueValue: Int) -> Bool {
+        var validation = false;
+        switch segueValue {
+        case 0:
+            validation = true
+            break
+        default:
+            validation = false
+            break
+        }
+        
+        return validation
     }
-    */
 
     /*
     // Override to support rearranging the table view.
